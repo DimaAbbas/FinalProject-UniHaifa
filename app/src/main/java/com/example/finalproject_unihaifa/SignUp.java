d836f320017ac9fa217241f1b559128a78a2fa02
DimaAbbas <49032206+DimaAbbas@users.noreply.github.com>
2021-08-15 19:10:12 +0300

    SignUp updating

--- app/src/main/java/com/example/finalproject_unihaifa/SignUp.java
+++ app/src/main/java/com/example/finalproject_unihaifa/SignUp.java
@@ -31,6 +31,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
     private FirebaseAuth mAuth;
     FirebaseDatabase database;
     DatabaseReference myRef, myAppointment;
+    User user;
 
 
     @Override
@@ -56,76 +57,74 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         myAppointment = database.getReference("Appointment Type");
     }
 
-    private void registerUser(){
+    private void registerUser() {
         Name = editName.getText().toString().trim();
         Phone = editPhone.getText().toString().trim();
         Email = editEmail.getText().toString().trim();
         Password = editPassword.getText().toString().trim();
         description = editDescription.getText().toString().trim();
 
-        if(Name.isEmpty()){
+        if (Name.isEmpty()) {
             editName.setError("Name is required!!");
             editName.requestFocus();
             return;
         }
-        if(Phone.isEmpty()){
+        if (Phone.isEmpty()) {
             editPhone.setError("Phone is required!!");
             editPhone.requestFocus();
             return;
         }
-        if(Phone.length() < 10){
+        if (Phone.length() < 10) {
             editPhone.setError("Please provide valid phone number!");
             editPhone.requestFocus();
             return;
         }
-        if(Email.isEmpty()){
+        if (Email.isEmpty()) {
             editEmail.setError("Email is required!!");
             editEmail.requestFocus();
             return;
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
             editEmail.setError("Please provide valid email");
             editEmail.requestFocus();
             return;
         }
-        if(Password.isEmpty()){
+        if (Password.isEmpty()) {
             editPassword.setError("Password is required!!");
             editPassword.requestFocus();
             return;
         }
-        if(Password.length() < 6){
+        if (Password.length() < 6) {
             editPassword.setError("Min password length should be 6 characters!");
             editPassword.requestFocus();
             return;
         }
-        if(rg.getCheckedRadioButtonId() == -1){
+        if (rg.getCheckedRadioButtonId() == -1) {
             Toast.makeText(getApplicationContext(), "Please select how you would like to use the app", Toast.LENGTH_SHORT).show();
             return;
-        }
-        else{
+        } else {
             userType = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
             UserType = userType.getText().toString().trim();
 
-            if(UserType.equals("Business Owner") && description.isEmpty()) {
+            if (UserType.equals("Business Owner") && description.isEmpty()) {
                 editDescription.setError("Add business description");
                 editDescription.requestFocus();
             }
         }
 
-        Query checkUser = myRef.orderByChild("name").equalTo(Name);
-        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(!snapshot.exists()){
-                    mAuth.createUserWithEmailAndPassword(Name,Password)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+        mAuth.createUserWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String uid = mAuth.getUid();
+                            Query checkUser = myRef.child(uid).orderByChild("name").equalTo(Name);
+                            checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if(task.isSuccessful()){
-                                        String uid = mAuth.getUid();
-                                        Toast.makeText(getApplicationContext(),"User Registered Successfully",Toast.LENGTH_LONG).show();
-                                        User user;
-                                        if (UserType.equals("Business Owner")){
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if (!snapshot.exists()) {
+                                        Toast.makeText(getApplicationContext(), "User Registered Successfully", Toast.LENGTH_LONG).show();
+                                        if (UserType.equals("Business Owner")) {
                                             user = new BusinessUser(Name, Phone, Email, Password, UserType, description);
                                             myAppointment.child(Name).setValue(Name);
                                         } else {
@@ -133,27 +132,26 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
                                         }
                                         myRef.child(uid).setValue(user);
                                         startActivity(new Intent(getApplicationContext(), LogIn.class));
-                                    }
-                                    else{
-                                        Toast.makeText(getApplicationContext(), "Failed to register! Try to change the email address", Toast.LENGTH_SHORT).show();
-
+                                    } else {
+                                        editName.setError("This username exists, select another one!");
+                                        editName.requestFocus();
+                                        return;
                                     }
                                 }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
                             });
-                }
-                else{
-                    editName.setError("This username exists, select another one!");
-                    editName.requestFocus();
-                    return;
-                }
-            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Failed to register! Change the email address", Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+                        }
+                    }
+                });
     }
+
     @Override
     public void onClick(View v) {
         switch(v.getId()){
